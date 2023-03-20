a1d1dac868dc0fdeb253a8a3bdf22c4c285e0664
ensofter <bogestvenniyiri@yandex.ru>
2023-03-20 20:29:30 +0300

    Add new parser

---
+++ new_parser.py
@@ -0,0 +1,97 @@
+import sys
+
+from pyparsing import Word, alphas, alphanums, Regex, Suppress, Optional, \
+    delimited_list, Keyword, restOfLine, quoted_string, ParseResults, \
+    Literal, LineStart
+
+
+ident = Word(alphas + "_", alphanums + "_").setName("identifier")
+integer = Regex(r"[+-]?\d+")
+
+LBRACE = Suppress('{')
+RBRACE = Suppress('}')
+LBRACK = Suppress('[')
+RBRACK = Suppress(']')
+LPAR = Suppress('(')
+RPAR = Suppress(')')
+EQ = Suppress('=')
+SEMI = Suppress(';')
+COLON = Suppress(':')
+RPC_ = Keyword('rpc')
+RETURNS_ = Keyword('returns')
+SERVICE_ = Literal('service')
+OPTION_ = Keyword('option')
+BODY_ = Keyword('body')
+STREAM_ = Keyword('stream')
+PACKAGE_ = Keyword('package')
+IMPORT_ = Keyword('import')
+TRUE_ = Keyword('true')
+COMMENT = '//' + restOfLine
+
+
+class Parser:
+
+    def __init__(self, proto_file: str):
+        self.proto_file = proto_file
+
+    def parsing_package_directive(self) -> str:
+        """ Парсим директиву package. """
+        package_directive = (
+                PACKAGE_
+                - delimited_list(ident, '.', combine=True)('PackageString')
+                + SEMI
+        )
+        package_directive.ignore(COMMENT)
+        result_of_parsing = package_directive.search_string(self.proto_file)
+        return result_of_parsing[0].as_dict()['PackageString']
+
+    def parsing_import_directives(self) -> ParseResults:
+        """ Парсим импорты внутри протника. """
+        import_directives = (
+                IMPORT_ - quoted_string('importPath') + SEMI
+        )
+        import_directives.ignore(COMMENT)
+        result_of_parsing = import_directives.search_string(self.proto_file)
+        return result_of_parsing
+
+    def parsing_service_name(self) -> str:
+        """ Парсим имя сервиса внутри протника. """
+        service_name = (
+                LineStart() + SERVICE_
+                - ident('serviceName')
+        )
+        service_name.ignore(COMMENT)
+        if result_of_parsing := service_name.search_string(self.proto_file):
+            return result_of_parsing[0].as_dict()['serviceName']
+        return ''
+
+    def parsing_service_handles(self) -> ParseResults:
+        """ Парсим всю директиву service. """
+        method_defn = (
+                RPC_
+                - ident("methodName")
+                + LPAR
+                + (Optional(STREAM_)('stream') + delimited_list(ident, '.', combine=True)("Request"))
+                + RPAR
+                + RETURNS_
+                + LPAR
+                + (Optional(STREAM_)('stream') + delimited_list(ident, '.', combine=True)("Response"))
+                + RPAR
+        )
+        method_defn.ignore(COMMENT)
+        result_of_parsing = method_defn.searchString(self.proto_file)
+
+        return result_of_parsing
+
+
+if __name__ == '__main__':
+    with open(sys.argv[1], 'r') as my_file:
+        proto_body = my_file.read()
+        parser = Parser(proto_body)
+        result = parser.parsing_service_handles()
+        print('!!!', type(result))
+        print('!!!', result)
+        for i in result:
+            print(i.as_dict())
+
+
--- old_parser.py
+++ old_parser.py
@@ -201,62 +201,6 @@ parser = ZeroOrMore(topLevelStatement)
 
 parser.ignore(comment)
 
-test_debugging = """
-syntax = "proto3";
-
-package ozon.whc.go.api.supplier_portal.supplier_api;
-
-option go_package = "gitlab.ozon.ru/whc/go/api/supplier-portal/pkg/supplier-api";
-
-import "google/api/annotations.proto";
-import "google/protobuf/timestamp.proto";
-import "github.com/envoyproxy/protoc-gen-validate/validate/validate.proto";
-
-service SupplierAPI {
-    rpc GetSupplyInfo (GetSupplyInfoRequest) returns (GetSupplyInfoResponse) {
-        option (google.api.http) = {
-            post: "/v1/supplier/supply-info"
-            body: "*"
-        };
-    };
-}
-
-message GetSupplyInfoRequest {
-    repeated int64 supply_ids = 1 [(validate.rules).repeated.unique = true, (validate.rules).repeated.items.int64.gt = 0];
-}
-
-message GetSupplyInfoResponse {
-    map<int64, Supply> supply_map = 1;
-
-    message Supply {
-        int64 supply_id = 1;
-        int64 supplier_id = 2;
-        string supplier_name = 3;
-        DocType type = 4;
-        Document document = 5;
-        repeated Item items = 6;
-
-        message Document {
-            string number = 2;
-            google.protobuf.Timestamp date = 3;
-        }
-
-        message Item {
-            int64 id = 1;
-            int64 quantity = 2;
-        }
-    }
-}
-
-enum DocType {
-    UNDEFINED = 0;
-    WITH_DOCS = 1;
-    WITHOUT_MARKING = 2;
-    WITH_MARKING = 3;
-}
-"""
-#parser.runTests([test_debugging])
-
 def imports_init(proto_name):
     imports = f"""from ozwhc.grpc_clients import BaseGrpc
 import {proto_name}_pb2, {proto_name}_pb2_grpc
@@ -303,14 +247,7 @@ def create_init_file(proto_name, service_name, methods):
 
 if __name__ == "__main__":
 
-    with open(sys.argv[1], 'r') as my_file:
-        proto_body = my_file.read().replace('\n', '')
-        result = parser.parseString(proto_body)
-        protofile_name = glob.glob("*.proto")[0][:-6]
-        for i in result:
-            i_as_dict = i.asDict()
-            if 'serviceName' in i_as_dict:
-                service_name = i_as_dict['serviceName']
-                if 'serviceMethods' in i_as_dict:
-                    methods = i_as_dict['serviceMethods']
-        create_init_file(protofile_name, service_name, methods)
+   with open(sys.argv[1], 'r') as my_file:
+       proto_body = my_file.read().replace('\n', '')
+       result = parser.parseString(proto_body)
+       print("!!!", result)
