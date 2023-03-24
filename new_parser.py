4e29154b542b3398443058ec3365c16c1fe955ae
ensofter <bogestvenniyiri@yandex.ru>
2023-03-24 17:43:58 +0300

    NewParser

--- new_parser.py
+++ new_parser.py
@@ -1,93 +1,108 @@
 import sys
+from abc import ABC, abstractmethod
 
-from pyparsing import Word, alphas, alphanums, Regex, Suppress, Optional, \
-    delimited_list, Keyword, restOfLine, quoted_string, ParseResults, \
-    Literal, LineStart, ZeroOrMore, Group
+from pyparsing import Word, alphas, alphanums, Suppress, Optional, delimited_list, Keyword, restOfLine, \
+    quoted_string, ParseResults, Literal, LineStart
+from pyparsing.core import ParserElement
 
-ident = Word(alphas + "_", alphanums + "_").setName("identifier")
-integer = Regex(r"[+-]?\d+")
 
-LBRACE = Suppress('{')
-RBRACE = Suppress('}')
-LBRACK = Suppress('[')
-RBRACK = Suppress(']')
+IDENT = Word(alphas + "_", alphanums + "_").setName("identifier")
 LPAR = Suppress('(')
 RPAR = Suppress(')')
 EQ = Suppress('=')
 SEMI = Suppress(';')
-COLON = Suppress(':')
 RPC_ = Keyword('rpc')
 RETURNS_ = Keyword('returns')
 SERVICE_ = Literal('service')
-OPTION_ = Keyword('option')
-BODY_ = Keyword('body')
 STREAM_ = Keyword('stream')
 PACKAGE_ = Keyword('package')
 IMPORT_ = Keyword('import')
-TRUE_ = Keyword('true')
 COMMENT = '//' + restOfLine
 
 
-class Parser:
+class IParser(ABC):
 
-    def __init__(self, proto_file: str):
-        self.proto_file = proto_file
+    def parse(self, parser_definition: ParserElement, proto_body: str):
+        parser_definition.ignore(COMMENT)
+        return parser_definition.search_string(proto_body)
 
-    def parsing_package_directive(self) -> str:
+    @abstractmethod
+    def package_directive(self):
+        pass
+
+    @abstractmethod
+    def import_directives(self):
+        pass
+
+    @abstractmethod
+    def service_name(self):
+        pass
+
+    @abstractmethod
+    def service_handlers(self):
+        pass
+
+
+class Parser(IParser):
+
+    def __init__(self, proto_body: str):
+        self.proto_body: str = proto_body
+
+    def package_directive(self) -> ParseResults:
         """ Парсим директиву package. """
         package_directive = (
                 PACKAGE_
-                - delimited_list(ident, '.', combine=True)('PackageString')
+                - delimited_list(IDENT, '.', combine=True)('PackageString')
                 + SEMI
         )
-        package_directive.ignore(COMMENT)
-        result_of_parsing = package_directive.search_string(self.proto_file)
-        return result_of_parsing[0].as_dict()['PackageString']
+        if result_of_parsing := self.parse(package_directive, self.proto_body):
+            return result_of_parsing[0].as_dict()['PackageString']
+        return result_of_parsing
 
-    def parsing_import_directives(self) -> ParseResults:
+    def import_directives(self) -> ParseResults:
         """ Парсим импорты внутри протника. """
         import_directives = (
                 IMPORT_ - quoted_string('importPath') + SEMI
         )
-        import_directives.ignore(COMMENT)
-        result_of_parsing = import_directives.search_string(self.proto_file)
-        return result_of_parsing
+        return self.parse(import_directives, self.proto_body)
 
-    def parsing_service_name(self) -> str:
+    def service_name(self) -> ParseResults:
         """ Парсим имя сервиса внутри протника. """
         service_name = (
                 LineStart() + SERVICE_
-                - ident('serviceName')
+                - IDENT('serviceName')
         )
-        service_name.ignore(COMMENT)
-        if result_of_parsing := service_name.search_string(self.proto_file):
+        if result_of_parsing := self.parse(service_name, self.proto_body):
             return result_of_parsing[0].as_dict()['serviceName']
-        return ''
+        return result_of_parsing
 
-    def parsing_service_handles(self) -> ParseResults:
+    def service_handlers(self) -> ParseResults:
         """ Парсим всю директиву service. """
         method_defn = (
                 RPC_
-                + ident("methodName")
+                + IDENT("methodName")
                 + LPAR
-                + (Optional(STREAM_)('stream') + delimited_list(ident, '.', combine=True)("Request"))
+                + (Optional(STREAM_)('stream') + delimited_list(IDENT, '.', combine=True)("Request"))
                 + RPAR
                 + RETURNS_
                 + LPAR
-                + (Optional(STREAM_)('stream') + delimited_list(ident, '.', combine=True)("Response"))
+                + (Optional(STREAM_)('stream') + delimited_list(IDENT, '.', combine=True)("Response"))
                 + RPAR
         )
-        method_defn.ignore(COMMENT)
-        result_of_parsing = method_defn.search_string(self.proto_file)
-
-        return result_of_parsing
+        return self.parse(method_defn, self.proto_body)
 
 
 if __name__ == '__main__':
     with open(sys.argv[1], 'r') as my_file:
         proto_body = my_file.read()
         parser = Parser(proto_body)
-        result = parser.parsing_service_handles()
+        result = parser.package_directive()
+        print('!!!', result)
+        result = parser.import_directives()
+        print('!!!', result)
+        result = parser.service_name()
+        print('!!!', result)
+        result = parser.service_handlers()
         print('!!!', result)
 
 
