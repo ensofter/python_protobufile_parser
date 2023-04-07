7932c36dc422abc6a2a630ffe46aad22c6508e7c
ensofter <bogestvenniyiri@yandex.ru>
2023-04-07 13:58:04 +0300

    Tests for get proto paths

--- tests/generator/test_get_proto_paths.py
+++ tests/generator/test_get_proto_paths.py
@@ -1,3 +1,4 @@
+import glob
 import os
 
 from generator import ProtoGenerator
@@ -6,13 +7,17 @@ from generator import ProtoGenerator
 class TestGetProtoPaths:
 
     def test_get_proto_paths(self):
-        path_to_mimir = '../mimir.yaml'
+        path_to_mimir = 'mimir.yaml'
         fp = open(path_to_mimir, 'w')
-        fp.write('proto_paths:\n\t- proto_files')
+        fp.write('proto_paths:\n    - proto_files')
         fp.close()
+
+        expected_proto_paths: list = glob.glob(f"proto_files/**/*.proto", recursive=True)
 
         proto_generator = ProtoGenerator()
         proto_paths = proto_generator.get_proto_paths(path_to_mimir)
-        print("!!!", proto_paths)
+
+        assert len(proto_paths) == len(expected_proto_paths)
+        assert sorted(proto_paths) == sorted(expected_proto_paths)
 
         os.remove(path_to_mimir)
